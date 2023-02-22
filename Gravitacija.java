245a78fec5b8960ca0babc45053222d88bc6c6d6
majkolman <maj.kolman98@gmail.com>
2023-02-22 19:37:54 +0100

    izracun

--- Gravitacija.java
+++ Gravitacija.java
@@ -5,7 +5,7 @@ public class Gravitacija {
         final double C = 6.674 * Math.pow(10, -11);
         final double M = 5.972 * Math.pow(10, 24);
         final double r = 6.371 * Math.pow(10, 6);
-
+        int a = 0;
         return (C*M)/((r+v)*(r+v));
     }
 }
